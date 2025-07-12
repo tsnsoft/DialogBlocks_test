789aefbe5b0cc55e89e9fd5ce7b3766bcada4ce8
Talipov S.N. <proffix4@gmail.com>
2025-07-12 14:32:56 +0500

    rev. 20250712_1432

--- git_push.bat
+++ git_push.bat
@@ -7,7 +7,7 @@ git add .
 for /f "tokens=1-3 delims=/.- " %%A in ("%date%") do set "datepart=%%C%%B%%A"
 for /f "tokens=1-2 delims=:" %%D in ("%time%") do set "timepart=%%D%%E"
 set "timepart=%timepart: =%"
-set "commit_msg=migration to wxWidgets 3.3
+set "commit_msg=rev. %datepart:~-8%_%timepart:~0,6%"
 
 echo Creating commit with message: %commit_msg%
 git commit -m "%commit_msg%"
